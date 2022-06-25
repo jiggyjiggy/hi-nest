9c20a18f6c39ecea0b75fab4b3ddb4f2ae83b762
jiggy <jiggyjiggy0323@gmail.com>
2022-06-25 18:22:16 +0900

    feat: Create update DTO

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entitiy';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -30,7 +31,7 @@ export class MoviesController {
     }
 
     @Patch(":id")
-    patch(@Param("id") movieId: number, @Body() updateData) {
+    patch(@Param("id") movieId: number, @Body() updateData: UpdateMovieDto) {
         return this.moviesService.update(movieId, updateData);
     }
 }
