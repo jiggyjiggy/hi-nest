0f23c9edffea79e0dd518014881ba828cabf4546
jiggy <jiggyjiggy0323@gmail.com>
2022-06-26 14:26:00 +0900

    test: patch, delete

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -48,6 +48,17 @@ describe('AppController (e2e)', () => {
       })
       .expect(201)
     });
+    it("POST 400", () => {
+      return request(app.getHttpServer())
+      .post("/movies")
+      .send({
+        title:"Test",
+        year: 2000,
+        genres:["test"],
+        other: 'thing'
+      })
+      .expect(400)
+    });
 
     it("DELETE", () => {
       return request(app.getHttpServer())
@@ -61,13 +72,26 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer())
         .get("/movies/1")
         .expect(200)
-      });
+      }
+    );
     it("GET 404", () => {
       return request(app.getHttpServer())
         .get("/movies/999")
         .expect(404)
-      });  
-    it.todo("DELETE");
-    it.todo("PATCH");
+      }
+    );  
+    it("PATCH", () => {
+      return request(app.getHttpServer())
+        .patch("/movies/1")
+        .send({title: "Updated Test"})
+        .expect(200)
+      }
+    );
+    it("DELETE", () => {
+      return request(app.getHttpServer())
+        .delete("/movies/1")
+        .expect(200)
+      }
+    );
   })
 });
